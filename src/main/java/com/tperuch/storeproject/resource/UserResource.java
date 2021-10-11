dd5847b836c22d6b34d92b947f222b2f25acfdfd
ThomasPeruch <peruchthomas@gmail.com>
2021-10-11 16:23:14 -0300

    get User by Id in controller and service

--- src/main/java/com/tperuch/storeproject/resource/UserResource.java
+++ src/main/java/com/tperuch/storeproject/resource/UserResource.java
@@ -5,6 +5,7 @@ import com.tperuch.storeproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class UserResource {
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
         return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
